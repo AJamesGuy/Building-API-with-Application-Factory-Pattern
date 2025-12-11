d1550a1ade4ff88570b24b7712327a1de4f779a7
Anthony Guy <anthonyyguyy@gmail.com>
2025-12-11 17:31:56 -0500

    Added required_token wrapper to customers routes bp. Created login route for customers_bp. Added limitation for logged in users on crud operations. Started swagger.yaml documentation.

--- app/blueprints/customers/routes.py
+++ app/blueprints/customers/routes.py
@@ -5,6 +5,9 @@ from marshmallow import ValidationError
 from app.models import Customer, db
 from app.extensions import limiter, cache
 from sqlalchemy import select
+from werkzeug.security import check_password_hash, generate_password_hash
+from app.util.auth import encode_token, token_required
+
 
 # ---------- CRUD operations for Customers ----------
 
@@ -15,15 +18,31 @@ def create_customer():
         data = customer_schema.load(request.json)
     except ValidationError as err:
         return jsonify(err.messages), 400
+    data['password'] = generate_password_hash(data['password'])
 
     new_customer = Customer(**data)
     db.session.add(new_customer)
     db.session.commit()
     return customer_schema.jsonify(new_customer), 201
 
+@customers_bp.route('/login', methods=['POST'])
+def login():
+    try:
+        data = customer_schema.load(request.json)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    customer = db.session.query(Customer).where(Customer.email == data['email']).first()
+
+    if customer and check_password_hash(customer.password, data['password']):
+        token = encode_token(customer.id)
+        return jsonify({"message": f"Welcome {customer.first_name}!",
+                        "token": token}), 200
+    return jsonify({"error": "Wrong email or password."})    
+
 # read all customers
 @customers_bp.route('/read-customers', methods=['GET'])
 @limiter.limit("10 per minute")
+@token_required
 def get_customers():
     try:
         page = int(request.args.get("page"))
@@ -36,7 +55,10 @@ def get_customers():
     
 # update customer
 @customers_bp.route('/<int:customer_id>/update-customer', methods=['PUT'])
+@token_required
 def update_customer(customer_id):
+    if request.logged_in_user_id != customer_id:
+        return jsonify({"error": "Unauthorized - you can only update your own account."}), 403
     customer = db.session.get(Customer, customer_id)
     if not customer:
         return jsonify({"error": "Customer not found"}), 404
@@ -54,7 +76,10 @@ def update_customer(customer_id):
 
 # delete a customer
 @customers_bp.route('/<int:customer_id>/delete-customer', methods=['DELETE'])
+@token_required
 def delete_customer(customer_id):
+    if request.logged_in_user_id != customer_id:
+        return jsonify({"error": "Unauthorized - you can only delete your own account."}), 403
     customer = db.session.get(Customer, customer_id)
     if not customer:
         return jsonify({"error": "Customer not found"}), 404
@@ -64,6 +89,7 @@ def delete_customer(customer_id):
     return jsonify({"message": "Customer deleted"}), 200
 
 @customers_bp.route('/search-customer', methods=['GET'])
+@token_required
 def search_customer():
     data = customer_schema.load(request.json)
 

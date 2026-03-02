d3176ba946a79f0e806e5a199018cbe435729180
Anthony Guy <anthonyyguyy@gmail.com>
2026-03-01 22:00:20 -0500

    Altered Production configuration to accomodate Render Web Service error

--- config.py
+++ config.py
@@ -14,6 +14,22 @@ class TestingConfig:
   TESTING = True
 
 class ProductionConfig:
-  SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///app.db' #Grabbing db url from a private environment
+  database_uri = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///app.db'
+  # Handle Render PostgreSQL SSL connection
+  if database_uri and 'postgres' in database_uri:
+    # Ensure we're using postgresql:// and add SSL parameters
+    database_uri = database_uri.replace('postgres://', 'postgresql://')
+    if '?' not in database_uri:
+      database_uri += '?sslmode=require'
+    elif 'sslmode' not in database_uri:
+      database_uri += '&sslmode=require'
+  SQLALCHEMY_DATABASE_URI = database_uri
+  SQLALCHEMY_ENGINE_OPTIONS = {
+    'pool_pre_ping': True,
+    'pool_recycle': 300,
+    'connect_args': {
+      'sslmode': 'require',
+    } if 'postgres' in database_uri else {}
+  }
   CACHE_TYPE = "SimpleCache"
   
